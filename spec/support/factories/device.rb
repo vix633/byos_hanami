0567423b718c3a386a7072a85ee08c6fcd3c00b5
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-07 09:51:24 -0700

    Added device factory
    Necessary to build a device for testing purposes.

---
+++ spec/support/factories/device.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Factory.define :device do |factory|
+  factory.friendly_id { "ABC123" }
+  factory.label { "Test" }
+  factory.api_key { "abc123" }
+  factory.mac_address { "aa:bb:cc:00:11:22" }
+  factory.created_at { Time.now }
+  factory.updated_at { Time.now }
+end
