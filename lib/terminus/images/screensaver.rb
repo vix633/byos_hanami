02fe92b42fe280ad49e52734940ff9382ab3d503
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-07 10:30:03 -0700

    Added image screensaver
    Necessary to take a screenshot of a web page and save it as an image for further processing to be eventually displayed on a device.
    
    Reek has been updated to ignore, for now, the feature envy and too many statements code smells because breaking out the logic for capturing an image doesn't make sense to extract further.

---
+++ lib/terminus/images/screensaver.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require "ferrum"
+
+module Terminus
+  module Images
+    # Saves web page as screenshot.
+    class Screensaver
+      SETTINGS = {
+        browser_options: {
+          "disable-dev-shm-usage" => nil,
+          "disable-gpu" => nil,
+          "hide-scrollbar" => nil,
+          "no-sandbox" => nil
+        },
+        js_errors: true
+      }.freeze
+
+      VIEWPORT = {width: 800, height: 480, scale_factor: 1}.freeze
+
+      def initialize settings: SETTINGS, browser: Ferrum::Browser
+        @settings = settings
+        @browser = browser
+      end
+
+      def call content, path, viewport: VIEWPORT
+        save content, path, viewport
+        path
+      end
+
+      private
+
+      attr_reader :settings, :browser
+
+      def save content, path, viewport
+        browser.new(settings).then do |instance|
+          instance.create_page
+          instance.set_viewport(**viewport)
+          instance.execute "document.documentElement.innerHTML = `#{content}`;"
+          instance.network.wait_for_idle
+          instance.screenshot path: path.to_s
+          instance.quit
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/terminus/images/screensaver_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require "hanami_helper"
+require "mini_magick"
+
+RSpec.describe Terminus::Images::Screensaver do
+  subject(:screensaver) { described_class.new }
+
+  include_context "with temporary directory"
+
+  describe "#call" do
+    let :content do
+      <<~CONTENT
+        <html>
+          <head>
+            <style>
+              color: black;
+              background-color: black;
+            </style>
+          </head>
+
+          <body>
+            <h1>Test</h1>
+          </body>
+        </html>
+      CONTENT
+    end
+
+    let(:path) { temp_dir.join "test.jpeg" }
+
+    it "creates screenshot" do
+      screensaver.call content, path
+      image = MiniMagick::Image.open path
+
+      expect(image).to have_attributes(width: 800, height: 480, type: "JPEG", exif: {})
+    end
+
+    it "answers image path" do
+      expect(screensaver.call(content, path)).to be(path)
+    end
+  end
+end
