7ee24356edaf5de9eca279d3e751a8cfa2a9ba7a
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-07 14:02:13 -0700

    Added devices show view
    Necessary to render a single device.

---
+++ app/views/devices/show.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Terminus
+  module Views
+    module Devices
+      # The show view.
+      class Show < Terminus::View
+        expose :device
+      end
+    end
+  end
+end
