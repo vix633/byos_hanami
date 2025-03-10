e901746b532c6038087e70169b4912e1c293d080
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-10 16:41:44 -0600

    Added API response display model
    Necessary to model data for the API display response. The default status set to 404 so you are forced to create a record that is valid.
    
    This will soon be used aby the corresponding Display API action once implemented.

---
+++ app/models/api/responses/display.rb
@@ -0,0 +1,40 @@
+# auto_register: false
+# frozen_string_literal: true
+
+module Terminus
+  module Models
+    module API
+      module Responses
+        # Models data for API display responses.
+        Display = Struct.new(
+          :filename,
+          :firmware_url,
+          :image_url,
+          :refresh_rate,
+          :reset_firmware,
+          :special_function,
+          :status,
+          :update_firmware
+        ) do
+          def initialize **arguments
+            super
+            apply_defaults
+            freeze
+          end
+
+          def to_json(*) = to_h.to_json(*)
+
+          private
+
+          def apply_defaults
+            self[:refresh_rate] ||= 300
+            self[:reset_firmware] ||= false
+            self[:update_firmware] ||= false
+            self[:special_function] ||= "sleep"
+            self[:status] ||= 404
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/app/models/api/responses/display_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require "hanami_helper"
+
+RSpec.describe Terminus::Models::API::Responses::Display do
+  subject(:model) { described_class.new }
+
+  describe "#initialize" do
+    it "answers default attributes" do
+      expect(model.to_h).to eq(
+        filename: nil,
+        firmware_url: nil,
+        image_url: nil,
+        refresh_rate: 300,
+        reset_firmware: false,
+        special_function: "sleep",
+        status: 404,
+        update_firmware: false
+      )
+    end
+
+    it "is frozen" do
+      expect(model.frozen?).to be(true)
+    end
+  end
+
+  describe "#to_json" do
+    it "answers JSON" do
+      payload = JSON model.to_json, symbolize_names: true
+
+      expect(payload).to eq(
+        filename: nil,
+        firmware_url: nil,
+        image_url: nil,
+        refresh_rate: 300,
+        reset_firmware: false,
+        special_function: "sleep",
+        status: 404,
+        update_firmware: false
+      )
+    end
+  end
+end
