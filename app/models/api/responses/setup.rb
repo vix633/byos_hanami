e3fc36fecdc974250c340a8fde926a5e202828a2
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-10 17:43:12 -0600

    Added API response setup model
    Necessary to model data for the API display response. The default status set to 404 so you are forced to create a record that is valid.
    
    This will soon be used aby the corresponding Setup API action once implemented.

---
+++ app/models/api/responses/setup.rb
@@ -0,0 +1,32 @@
+# auto_register: false
+# frozen_string_literal: true
+
+module Terminus
+  module Models
+    module API
+      module Responses
+        # Models data for API setup responses.
+        Setup = Struct.new :api_key, :friendly_id, :image_url, :message, :status do
+          def self.for device, environment: ENV
+            new api_key: device.api_key,
+                friendly_id: device.friendly_id,
+                image_url: %(#{environment.fetch "APP_URL"}/images/setup/logo.bmp),
+                message: "Welcome to TRMNL BYOS",
+                status: 200
+          end
+
+          def initialize **arguments
+            super
+
+            self[:message] ||= "MAC Address not registered."
+            self[:status] ||= 404
+
+            freeze
+          end
+
+          def to_json(*) = to_h.to_json(*)
+        end
+      end
+    end
+  end
+end
---
+++ spec/app/models/api/responses/setup_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require "hanami_helper"
+
+RSpec.describe Terminus::Models::API::Responses::Setup, :db do
+  subject(:model) { described_class.new }
+
+  describe ".for" do
+    it "answers record for device" do
+      device = Factory[:device]
+
+      expect(described_class.for(device)).to eq(
+        described_class[
+          api_key: "abc123",
+          friendly_id: "ABC123",
+          image_url: %(#{ENV.fetch "APP_URL"}/images/setup/logo.bmp),
+          message: "Welcome to TRMNL BYOS",
+          status: 200
+        ]
+      )
+    end
+  end
+
+  describe "#initialize" do
+    it "answers default attributes" do
+      expect(model.to_h).to eq(
+        api_key: nil,
+        friendly_id: nil,
+        image_url: nil,
+        message: "MAC Address not registered.",
+        status: 404
+      )
+    end
+
+    it "is frozen" do
+      expect(model.frozen?).to be(true)
+    end
+  end
+
+  describe "#to_json" do
+    it "answers JSON" do
+      payload = JSON model.to_json, symbolize_names: true
+
+      expect(payload).to eq(
+        api_key: nil,
+        friendly_id: nil,
+        image_url: nil,
+        message: "MAC Address not registered.",
+        status: 404
+      )
+    end
+  end
+end
