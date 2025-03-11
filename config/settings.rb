93392213e96ad3f4267efc731148e82971d70bf6
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-10 19:49:25 -0600

    Added app settings
    Necessary to define default settings for use throughout the application.

--- config/settings.rb
+++ config/settings.rb
@@ -3,5 +3,8 @@
 module Terminus
   # The application base settings.
   class Settings < Hanami::Settings
+    setting :app_host, constructor: Types::Params::String
+    setting :app_url, constructor: Types::Params::String
+    setting :images_root, constructor: Types::Params::String
   end
 end
