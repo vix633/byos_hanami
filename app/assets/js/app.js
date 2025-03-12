c1ba3e7e18c385257bc2484f7ff663538e0f5045
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-12 12:04:07 -0600

    Updated assets entrypoint include new CSS styles
    Necessary so Hanami will build our CSS assets as a single bundle.
    
    Milestone: minor

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,1 +1,7 @@
-import "../css/app.css";
+import "../css/settings.css";
+import "../css/colors.css";
+import "../css/view_transitions.css";
+import "../css/defaults.css";
+import "../css/layout.css";
+import "../css/components.css";
+import "../css/devices.css";
