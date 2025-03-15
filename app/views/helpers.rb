d1094d695c3cec999be21ffe4cd2fabdb01fc47a
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-15 10:13:58 -0600

    Added view helpers
    Necessary for rendering device specific HTML elements. This mostly makes use of the HTMX gem.
    
    Milestone: minor

---
+++ app/views/helpers.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+require "htmx"
+require "refinements/hash"
+
+module Terminus
+  module Views
+    # The view helpers.
+    module Helpers
+      using Refinements::Hash
+
+      def search path
+        tag.input(
+          id: "search",
+          type: "search",
+          name: "query",
+          value: "",
+          **HTMX[
+            get: path,
+            trigger: "search, keyup delay:200ms changed",
+            target: "next .tasks",
+            push_url: true,
+            indicator: ".loader"
+          ]
+        )
+      end
+
+      # :reek:UtilityFunction
+      def field_for key, attributes, record = nil
+        return attributes[key] unless record
+
+        attributes.fetch_value key, record.public_send(key)
+      end
+
+      def device_new
+        path = routes.path :devices_new
+
+        link_to(
+          "New",
+          path,
+          class: "button button-new",
+          **HTMX[
+            get: path,
+            trigger: "click, keyup[ctrlKey&&key=='n'] from:body",
+            target: ".devices",
+            swap: "beforeend settle:0.1s"
+          ]
+        )
+      end
+
+      def device_save verb, path
+        tag.input(
+          type: :submit,
+          value: "Save",
+          class: :accept,
+          **HTMX[
+            verb => path,
+            trigger: "click, keyup[ctrlKey&&key=='s'] from:closest .device",
+            target: "closest .device",
+            swap: "outerHTML swap:0s"
+          ]
+        )
+      end
+
+      def device_form_cancel path
+        tag.input(
+          type: :button,
+          value: "Cancel",
+          class: :decline,
+          **HTMX[
+            get: path,
+            trigger: "click",
+            target: "closest .device",
+            swap: "outerHTML swap:0s"
+          ]
+        )
+      end
+
+      def device_form_remove
+        tag.input type: :button, value: "Cancel", class: :decline, data: {remove: true}
+      end
+    end
+  end
+end
