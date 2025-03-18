742c9d64a3c23e73a1fc8e7627bf1275dd2bcec0
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-18 13:43:15 -0600

    Removed home slice configuration
    No longer necessary since the home slice has been removed.
    
    Milestone: patch

--- config/slices/home.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Home
-  # The home slice configuration.
-  class Slice < Hanami::Slice
-    import keys: ["assets"], from: Hanami.app.container, as: :app
-  end
-end
