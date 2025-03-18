4f13a0b97b4c2a0395df48eb4d4740aebbab9c34
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-18 14:07:26 -0600

    Added devices info show view
    Necessary to render device information.
    
    Milestone: minor

---
+++ app/views/devices/info/show.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Terminus
+  module Views
+    module Devices
+      module Info
+        # The show view.
+        class Show < Terminus::View
+          expose :device
+        end
+      end
+    end
+  end
+end
