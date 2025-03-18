26360c546256989069a5d17d9aa49e4c7b01a05b
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-18 10:12:05 -0600

    Added device part battery and signal percentage
    Necessary to render the correct percentages.
    
    Both RuboCop and Reek were updated to ignore method length, complexity, and number of statements because there's no better way to represent this logic except within a single case statement which is what we want in this case.
    
    Milestone: minor

--- app/views/parts/device.rb
+++ app/views/parts/device.rb
@@ -11,6 +11,38 @@ module Terminus
         include Initable[fetcher: proc { Terminus::Images::Fetcher.new }]
         include Deps[:settings]
 
+        def battery_percentage
+          case battery
+            when 0 then 0
+            when ..0.45 then 10
+            when 0.46..0.9 then 20
+            when 1.0..1.35 then 30
+            when 1.36..1.8 then 40
+            when 1.81..2.25 then 50
+            when 2.26..2.7 then 60
+            when 2.71..3.15 then 70
+            when 3.16..3.6 then 80
+            when 3.61..4.05 then 90
+            else 100
+          end
+        end
+
+        def signal_percentage
+          case signal
+            when 0 then 0
+            when ..-91 then 10
+            when -90..-81 then 20
+            when -80..-71 then 30
+            when -70..-67 then 40
+            when -66..-62 then 50
+            when -61..-57 then 60
+            when -56..-52 then 70
+            when -51..-47 then 80
+            when -46..-40 then 90
+            else 100
+          end
+        end
+
         def image_uri
           fetcher.call(
             Pathname(settings.images_root).join("generated"),
--- spec/app/views/parts/device_spec.rb
+++ spec/app/views/parts/device_spec.rb
@@ -31,4 +31,133 @@ RSpec.describe Terminus::Views::Parts::Device, :db do
       expect(part.image_uri).to eq("https://localhost:2443/assets/images/generated/test.bmp")
     end
   end
+
+  describe "#battery_percentage" do
+    it "answers zero when zero" do
+      allow(device).to receive(:battery).and_return(0)
+      expect(part.battery_percentage).to eq(0)
+    end
+
+    it "answers ten percent when extremely low" do
+      allow(device).to receive(:battery).and_return(0.1)
+      expect(part.battery_percentage).to eq(10)
+    end
+
+    it "answers ten percent when in range" do
+      allow(device).to receive(:battery).and_return(0.25)
+      expect(part.battery_percentage).to eq(10)
+    end
+
+    it "answers twenty percent when in range" do
+      allow(device).to receive(:battery).and_return(0.75)
+      expect(part.battery_percentage).to eq(20)
+    end
+
+    it "answers thirty percent when in range" do
+      allow(device).to receive(:battery).and_return(1.15)
+      expect(part.battery_percentage).to eq(30)
+    end
+
+    it "answers fourty percent when in range" do
+      allow(device).to receive(:battery).and_return(1.5)
+      expect(part.battery_percentage).to eq(40)
+    end
+
+    it "answers fifty percent when in range" do
+      allow(device).to receive(:battery).and_return(2.0)
+      expect(part.battery_percentage).to eq(50)
+    end
+
+    it "answers sixty percent when in range" do
+      allow(device).to receive(:battery).and_return(2.5)
+      expect(part.battery_percentage).to eq(60)
+    end
+
+    it "answers seventy percent when in range" do
+      allow(device).to receive(:battery).and_return(3.0)
+      expect(part.battery_percentage).to eq(70)
+    end
+
+    it "answers eighty percent when in range" do
+      allow(device).to receive(:battery).and_return(3.3)
+      expect(part.battery_percentage).to eq(80)
+    end
+
+    it "answers ninety percent when in range" do
+      allow(device).to receive(:battery).and_return(3.9)
+      expect(part.battery_percentage).to eq(90)
+    end
+
+    it "answers one hundred percent when in range" do
+      allow(device).to receive(:battery).and_return(4.8)
+      expect(part.battery_percentage).to eq(100)
+    end
+
+    it "answers one hundred percent beyond high end range" do
+      allow(device).to receive(:battery).and_return(4.5)
+      expect(part.battery_percentage).to eq(100)
+    end
+  end
+
+  describe "#signal_percentage" do
+    it "answers zero when zero" do
+      allow(device).to receive(:signal).and_return(0)
+      expect(part.battery_percentage).to eq(0)
+    end
+
+    it "answers ten percent when extremely low" do
+      allow(device).to receive(:signal).and_return(-100)
+      expect(part.signal_percentage).to eq(10)
+    end
+
+    it "answers ten percent when in range" do
+      allow(device).to receive(:signal).and_return(-95)
+      expect(part.signal_percentage).to eq(10)
+    end
+
+    it "answers twenty percent when in range" do
+      allow(device).to receive(:signal).and_return(-85)
+      expect(part.signal_percentage).to eq(20)
+    end
+
+    it "answers thirty percent when in range" do
+      allow(device).to receive(:signal).and_return(-75)
+      expect(part.signal_percentage).to eq(30)
+    end
+
+    it "answers fourty percent when in range" do
+      allow(device).to receive(:signal).and_return(-69)
+      expect(part.signal_percentage).to eq(40)
+    end
+
+    it "answers fifty percent when in range" do
+      allow(device).to receive(:signal).and_return(-65)
+      expect(part.signal_percentage).to eq(50)
+    end
+
+    it "answers sixty percent when in range" do
+      allow(device).to receive(:signal).and_return(-59)
+      expect(part.signal_percentage).to eq(60)
+    end
+
+    it "answers seventy percent when in range" do
+      allow(device).to receive(:signal).and_return(-54)
+      expect(part.signal_percentage).to eq(70)
+    end
+
+    it "answers eighty percent when in range" do
+      allow(device).to receive(:signal).and_return(-49)
+      expect(part.signal_percentage).to eq(80)
+    end
+
+    it "answers ninety percent when in range" do
+      allow(device).to receive(:signal).and_return(-45)
+      expect(part.signal_percentage).to eq(90)
+    end
+
+    it "answers one hundred percent when in range" do
+      allow(device).to receive(:signal).and_return(-25)
+      expect(part.signal_percentage).to eq(100)
+    end
+  end
 end
