413747549c87165621a7b55f29bf364c6710b03d
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-19 11:55:49 -0600

    Added dashboard show action loading of IPs and devices
    Necessary to provide basic high level information and summaries of what is supported by the server.
    
    Milestone: minor

--- app/actions/dashboard/show.rb
+++ app/actions/dashboard/show.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
+require "initable"
+
 module Terminus
   module Actions
     module Dashboard
       # The show action.
       class Show < Terminus::Action
-        def handle(*) = super
+        include Deps[:settings, device_repository: "repositories.device"]
+        include Initable[ip_finder: proc { Terminus::IPFinder.new }]
+
+        def handle _request, response
+          response.render view,
+                          devices: device_repository.all,
+                          ip_addresses: ip_finder.all,
+                          api_uri: settings.api_uri
+        end
       end
     end
   end
