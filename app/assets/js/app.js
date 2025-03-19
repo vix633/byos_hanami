9662151438c2416f9e4ae0fd2691d13783452410
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-19 11:42:58 -0600

    Added dashboard stylesheet
    Necessary to style dashboard widgets for a high level overview.
    
    Milestone: minor

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -4,4 +4,5 @@ import "../css/view_transitions.css";
 import "../css/defaults.css";
 import "../css/layout.css";
 import "../css/components.css";
+import "../css/dashboard.css";
 import "../css/devices.css";
