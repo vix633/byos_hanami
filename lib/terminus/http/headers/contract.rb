2f5a2b1f540dd31ab0becdffef89952335d38630
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-19 18:53:27 -0600

    Updated HTTP headers contract to relax required keys
    Necessary to allow non-device specific API calls.
    
    Milestone: patch

--- lib/terminus/http/headers/contract.rb
+++ lib/terminus/http/headers/contract.rb
@@ -5,15 +5,15 @@ module Terminus
     module Headers
       Contract = Dry::Schema.Params do
         required(:HTTP_ACCESS_TOKEN).filled :string
-        required(:HTTP_BATTERY_VOLTAGE).filled :float
-        required(:HTTP_FW_VERSION).filled Types::Version
-        required(:HTTP_HOST).filled :string
+        optional(:HTTP_BATTERY_VOLTAGE).filled :float
+        optional(:HTTP_FW_VERSION).filled Types::Version
+        optional(:HTTP_HOST).filled :string
         required(:HTTP_ID).filled :string
-        required(:HTTP_REFRESH_RATE).filled :integer
-        required(:HTTP_RSSI).filled :integer
-        required(:HTTP_USER_AGENT).filled :string
-        required(:HTTP_WIDTH).filled :integer
-        required(:HTTP_HEIGHT).filled :integer
+        optional(:HTTP_REFRESH_RATE).filled :integer
+        optional(:HTTP_RSSI).filled :integer
+        optional(:HTTP_USER_AGENT).filled :string
+        optional(:HTTP_WIDTH).filled :integer
+        optional(:HTTP_HEIGHT).filled :integer
       end
     end
   end
