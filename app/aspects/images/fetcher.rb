53902099fa24d058401022ff4e02acf4550eae8a
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-21 09:43:49 -0600

    Updated image fetcher to be an application aspect
    Necessary to leverage application specific settings and use those for safe defaults. This'll help improve the command line usages as well.
    
    Milestone: minor

---
+++ app/aspects/images/fetcher.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require "base64"
+require "initable"
+require "refinements/pathname"
+
+module Terminus
+  module Aspects
+    module Images
+      # Fetches image for rendering on device screen.
+      class Fetcher
+        include Initable[encryptions: [:base_64]]
+        include Deps[:settings, :assets]
+
+        using Refinements::Pathname
+
+        def call images_uri:, encryption: nil
+          last_generated_image(images_uri, encryption) || default
+        end
+
+        private
+
+        # :reek:TooManyStatements
+        def last_generated_image images_uri, encryption
+          image_path = root_path.files("*.bmp").max_by(&:ctime)
+
+          return unless image_path
+
+          filename = image_path.basename.to_s
+
+          image_url = if encryptable? encryption
+                        "data:image/bmp;base64,#{Base64.strict_encode64 image_path.read}"
+                      else
+                        "#{images_uri}/generated/#{filename}"
+                      end
+
+          {filename:, image_url:}
+        end
+
+        def encryptable?(value) = encryptions.include?(value) && value == :base_64
+
+        def root_path = Pathname settings.generated_root
+
+        def default = {filename: "empty_state", image_url: assets["setup.bmp"].url}
+      end
+    end
+  end
+end
---
+++ spec/app/aspects/images/fetcher_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require "hanami_helper"
+
+RSpec.describe Terminus::Aspects::Images::Fetcher do
+  using Refinements::Pathname
+
+  subject(:fetcher) { described_class.new settings: }
+
+  include_context "with temporary directory"
+
+  let(:settings) { Hanami.app[:settings] }
+
+  before { allow(settings).to receive(:generated_root).and_return temp_dir }
+
+  describe "#call" do
+    let(:fixture_path) { SPEC_ROOT.join "support/fixtures/test.bmp" }
+    let(:images_uri) { "https://localhost/assets" }
+
+    it "answers default image" do
+      expect(fetcher.call(images_uri:)).to match(
+        filename: "empty_state",
+        image_url: %r(/assets/setup.*\.bmp)
+      )
+    end
+
+    it "answers generated image without encryption" do
+      path = temp_dir.join "test.bmp"
+      fixture_path.copy path
+
+      expect(fetcher.call(images_uri:)).to eq(
+        filename: "test.bmp",
+        image_url: "https://localhost/assets/generated/test.bmp"
+      )
+    end
+
+    it "answers generated image with encryption" do
+      path = temp_dir.join "test.bmp"
+      fixture_path.copy path
+
+      expect(fetcher.call(images_uri:, encryption: :base_64)).to match(
+        filename: "test.bmp",
+        image_url: %r(data:image/bmp;base64,.+)
+      )
+    end
+
+    it "answers generated image without encryption when given invalid encryption" do
+      path = temp_dir.join "test.bmp"
+      fixture_path.copy path
+
+      expect(fetcher.call(images_uri:, encryption: :bogus)).to eq(
+        filename: "test.bmp",
+        image_url: "https://localhost/assets/generated/test.bmp"
+      )
+    end
+  end
+end
