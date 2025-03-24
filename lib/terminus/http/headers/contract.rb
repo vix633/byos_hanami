93a2475559ecdea56968d5ec277be25c9d54512c
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-24 10:00:03 -0600

    Fixed HTTP header contract to maybe have access token
    Necessary to prevent setup failure after device is automatically provisioned. This also happens on API display requests now where the access token is always blank now.
    
    See link:https://github.com/usetrmnl/firmware/issues/96[Firmware Issue 96] for additional details.
    
    Milestone: patch

--- lib/terminus/http/headers/contract.rb
+++ lib/terminus/http/headers/contract.rb
@@ -4,7 +4,7 @@ module Terminus
   module HTTP
     module Headers
       Contract = Dry::Schema.Params do
-        required(:HTTP_ACCESS_TOKEN).filled :string
+        required(:HTTP_ACCESS_TOKEN).maybe :string
         optional(:HTTP_BATTERY_VOLTAGE).filled :float
         optional(:HTTP_FW_VERSION).filled Types::Version
         optional(:HTTP_HOST).filled :string
