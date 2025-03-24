a5f1ea28be1dee75029c6760483d243c58143d0a
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-24 10:18:48 -0600

    Added API setup show action firmware version capture
    We can capture the firmware version early (rather than waiting to update the device during API device requests) when auto-provisioning a new device. This is possible because the firmware will include the version in the header now.
    
    Milestone: minor

--- app/actions/api/setup/show.rb
+++ app/actions/api/setup/show.rb
@@ -15,19 +15,20 @@ module Terminus
           format :json
 
           def handle request, response
-            mac_address = request.env["HTTP_ID"]
+            mac_address, firmware_version = request.env.values_at "HTTP_ID", "HTTP_FW_VERSION"
             device = repository.find_by_mac_address mac_address
-            device ||= create_device mac_address
+            device ||= create_device mac_address, firmware_version
             response.body = model.for(device).to_json
           end
 
           private
 
-          def create_device mac_address
+          def create_device mac_address, firmware_version
             repository.create label: "TRMNL",
                               friendly_id: generate_friendly_id,
                               mac_address:,
                               api_key: generate_api_key,
+                              firmware_version:,
                               setup_at: Time.now
           end
 
--- spec/app/actions/api/setup/show_spec.rb
+++ spec/app/actions/api/setup/show_spec.rb
@@ -11,11 +11,16 @@ RSpec.describe Terminus::Actions::API::Setup::Show, :db do
 
     context "with no devices" do
       let(:mac_address) { "aa:bb:cc:00:11:22" }
-      let(:response) { Rack::MockRequest.new(action).get "/api/setup", "HTTP_ID" => mac_address }
       let(:payload) { JSON response.body, symbolize_names: true }
       let(:device) { repository.find_by_mac_address mac_address }
 
-      it "registers new device for MAC address" do
+      let :response do
+        Rack::MockRequest.new(action).get "/api/setup",
+                                          "HTTP_ID" => mac_address,
+                                          "HTTP_FW_VERSION" => "1.2.3"
+      end
+
+      it "answers device/image details for new device" do
         expect(payload).to eq(
           api_key: device.api_key,
           friendly_id: device.friendly_id,
@@ -24,13 +29,14 @@ RSpec.describe Terminus::Actions::API::Setup::Show, :db do
         )
       end
 
-      it "creates device with generated defaults" do
+      it "creates device" do
         response
 
         expect(device).to have_attributes(
           label: "TRMNL",
           friendly_id: /[A-Z0-9]{6}/,
           mac_address:,
+          firmware_version: "1.2.3",
           api_key: /[a-z0-9]{20}/i,
           setup_at: instance_of(Time)
         )
