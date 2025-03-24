f01a882d8c2bff41a043030e0742ea12e7da0e89
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-24 14:34:06 -0600

    Added API log payload processing
    Necessary to capture log information for devices in order to improve the debugability of individual devices.
    
    This replaces the original HTTP header dump to the logs in favor of logging what we get back from the device straight to the database. Each log is associated with a device. This information will eventually be used for display within the UI.
    
    Milestone: minor

--- app/actions/api/log/create.rb
+++ app/actions/api/log/create.rb
@@ -6,33 +6,66 @@ module Terminus
       module Log
         # The create action.
         class Create < Terminus::Action
-          include Deps[:logger]
+          include Deps[
+            :logger,
+            device_repository: "repositories.device",
+            log_repository: "repositories.device_log"
+          ]
 
-          KEYS = %w[
-            SERVER_SOFTWARE
-            SERVER_NAME
-            SERVER_PORT
-            HTTP_BASE64
-            HTTP_FW_VERSION
-            HTTP_ACCESS_TOKEN
-            HTTP_HEIGHT
-            HTTP_HOST
-            HTTP_ID
-            HTTP_REFRESH_RATE
-            HTTP_USER_AGENT
-            HTTP_WIDTH
-            REQUEST_METHOD
-            REQUEST_PATH
-            REQUEST_URI
-            QUERY_STRING
-            PATH_INFO
-          ].freeze
+          include Initable[
+            transformer: proc { Terminus::Aspects::API::Transformers::FirmwareLog.new }
+          ]
 
           format :json
 
+          params do
+            required(:log).hash do
+              required(:logs_array).array(:hash) do
+                required(:device_status_stamp).filled(:hash) do
+                  required(:wifi_rssi_level).filled :integer
+                  required(:wifi_status).filled :string
+                  required(:refresh_rate).filled :integer
+                  required(:time_since_last_sleep_start).filled :integer
+                  required(:current_fw_version).filled :string
+                  required(:special_function).filled :string
+                  required(:battery_voltage).filled :float
+                  required(:wakeup_reason).filled :string
+                  required(:free_heap_size).filled :integer
+                end
+                optional(:additional_info).filled(:hash) do
+                  optional(:retry_attempt).filled :integer
+                end
+                required(:creation_timestamp).filled :integer
+                required(:log_id).filled :integer
+                required(:log_message).filled :string
+                required(:log_codeline).filled :integer
+                required(:log_sourcefile).filled :string
+              end
+            end
+          end
+
+          # :reek:FeatureEnvy
           def handle request, response
-            logger.info(**request.env.slice(*KEYS))
+            environment = request.env
+            parameters = request.params
+
+            save environment, parameters
+
             response.status = 204
+          end
+
+          private
+
+          def save environment, parameters
+            if parameters.valid?
+              device = device_repository.find_by_mac_address environment["HTTP_ID"]
+
+              transformer.call(parameters.to_h).each do |attributes|
+                log_repository.create attributes.merge!(device_id: device.id)
+              end
+            else
+              logger.error parameters.errors
+            end
           end
         end
       end
--- spec/app/actions/api/log/create_spec.rb
+++ spec/app/actions/api/log/create_spec.rb
@@ -2,7 +2,7 @@
 
 require "hanami_helper"
 
-RSpec.describe Terminus::Actions::API::Log::Create do
+RSpec.describe Terminus::Actions::API::Log::Create, :db do
   subject(:action) { described_class.new logger: }
 
   let(:logger) { instance_spy Dry::Logger::Dispatcher }
@@ -10,33 +10,72 @@ RSpec.describe Terminus::Actions::API::Log::Create do
   describe "#call" do
     let :headers do
       {
-        "SERVER_SOFTWARE" => "Test 0.0.0",
-        "SERVER_NAME" => "test.io",
-        "SERVER_PORT" => 80,
         "HTTP_BASE64" => "true",
         "HTTP_FW_VERSION" => "0.0.0",
         "HTTP_ACCESS_TOKEN" => "abc123",
         "HTTP_HEIGHT" => 480,
         "HTTP_HOST" => "test.io",
-        "HTTP_ID" => "test",
+        "HTTP_ID" => device.mac_address,
         "HTTP_REFRESH_RATE" => 100,
         "HTTP_USER_AGENT" => "test",
-        "HTTP_WIDTH" => 800,
-        "REQUEST_METHOD" => "POST",
-        "REQUEST_PATH" => "/",
-        "REQUEST_URI" => "http://test.io",
-        "QUERY_STRING" => "",
-        "PATH_INFO" => "/"
+        "HTTP_WIDTH" => 800
       }
     end
 
-    it "logs details" do
-      Rack::MockRequest.new(action).get "/api/display", headers
-      expect(logger).to have_received(:info).with(headers)
+    let :payload do
+      {
+        log: {
+          logs_array: [
+            {
+              device_status_stamp: {
+                battery_voltage: 4.772,
+                current_fw_version: "1.2.3",
+                free_heap_size: 160656,
+                refresh_rate: 30,
+                special_function: "none",
+                time_since_last_sleep_start: 31,
+                wakeup_reason: "timer",
+                wifi_rssi_level: -54,
+                wifi_status: "connected"
+              },
+              additional_info: {
+                retry_attempt: 1
+              },
+              log_codeline: 597,
+              log_id: 2,
+              log_message: "Danger!",
+              log_sourcefile: "src/bl.cpp",
+              creation_timestamp: 1742000523
+            }
+          ]
+        }
+      }
+    end
+
+    let(:device) { Factory[:device] }
+    let(:repository) { Hanami.app["repositories.device_log"] }
+
+    it "create record" do
+      # FIX: Using GET to trick payload processing. Not ideal.
+      Rack::MockRequest.new(action).get "", **headers, params: payload
+
+      expect(repository.all.first).to have_attributes(
+        device_id: device.id,
+        external_id: 2,
+        message: "Danger!",
+        special_function: "none",
+        retry: 1,
+        created_at: Time.utc(2025, 3, 15, 1, 2, 3)
+      )
+    end
+
+    it "logs errors when parameters are invalid" do
+      Rack::MockRequest.new(action).post "/api/log", headers
+      expect(logger).to have_received(:error).with(log: ["is missing"])
     end
 
     it "answers success (no content)" do
-      response = Rack::MockRequest.new(action).get "/api/display", headers
+      response = Rack::MockRequest.new(action).post "/api/log", headers
       expect(response.status).to eq(204)
     end
   end
