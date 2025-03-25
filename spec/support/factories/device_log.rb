29ae1b188f3edefe71224d77ea7a87b00d8cb045
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-25 16:01:00 -0600

    Added device log factory
    Necessary for testing purposes.
    
    Milestone: minor

---
+++ spec/support/factories/device_log.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+Factory.define :device_log do |factory|
+  factory.association :device
+  factory.sequence(:external_id) { it }
+  factory.message "Danger!"
+  factory.source_path "src/test.cpp"
+  factory.source_line 13
+  factory.retry 3
+  factory.wifi_signal(-33)
+  factory.wifi_status "connected"
+  factory.refresh_rate 50
+  factory.sleep_duration 25
+  factory.firmware_version "1.2.3"
+  factory.special_function "none"
+  factory.wake_reason "timer"
+  factory.battery_voltage 4.75
+  factory.free_heap_size 150000
+  factory.created_at { Time.now }
+  factory.updated_at { Time.now }
+end
