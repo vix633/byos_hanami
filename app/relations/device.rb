a3983f2189f66e59a46665b465fc2659b21be72f
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-25 15:34:39 -0600

    Added device relation logs association
    Necessary to acquire logs for a device.
    
    Milestone: minor

--- app/relations/device.rb
+++ app/relations/device.rb
@@ -4,7 +4,9 @@ module Terminus
   module Relations
     # The device relation.
     class Device < DB::Relation
-      schema :devices, infer: true
+      schema :devices, infer: true do
+        associations { has_many :device_logs, as: :logs }
+      end
     end
   end
 end
