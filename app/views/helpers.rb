16d199ec81e9ec4fbb7ab12c2742a6cb03d79e1f
Brooke Kuhlmann <brooke@alchemists.io>
2025-03-27 16:05:15 -0600

    Removed device helpers
    No longer necessary to maintain since this logic has been moved into partials.
    
    Milestone: minor

--- app/views/helpers.rb
+++ app/views/helpers.rb
@@ -13,54 +13,6 @@ module Terminus
 
       module_function
 
-      def device_new
-        path = routes.path :devices_new
-
-        link_to(
-          "New",
-          path,
-          class: "button button-new",
-          **HTMX[
-            get: path,
-            trigger: "click, keyup[ctrlKey&&key=='n'] from:body",
-            target: ".devices",
-            swap: "beforeend settle:0.1s"
-          ]
-        )
-      end
-
-      def device_form_cancel path
-        tag.input(
-          type: :button,
-          value: "Cancel",
-          class: :decline,
-          **HTMX[
-            get: path,
-            trigger: "click",
-            target: "closest .device",
-            swap: "outerHTML swap:0s"
-          ]
-        )
-      end
-
-      def device_form_remove
-        tag.input type: :button, value: "Cancel", class: :decline, data: {remove: true}
-      end
-
-      def device_save verb, path
-        tag.input(
-          type: :submit,
-          value: "Save",
-          class: :accept,
-          **HTMX[
-            verb => path,
-            trigger: "click, keyup[ctrlKey&&key=='s'] from:closest .device",
-            target: "closest .device",
-            swap: "outerHTML swap:0s"
-          ]
-        )
-      end
-
       def field_for key, attributes, record = nil
         return attributes[key] unless record
 
