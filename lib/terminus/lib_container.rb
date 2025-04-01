ce615102dca8bcad15766f6434e10680878bfcdf
Brooke Kuhlmann <brooke@alchemists.io>
2025-04-01 09:09:44 -0600

    Added application container
    Necessary to register dependencies for potential injection. Using `LibContainer` as the container name, which is slightly awkward, in order to not conflict with the `Terminus::Container` provided by Hanami.
    
    Milestone: minor

---
+++ lib/terminus/lib_container.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "cogger"
+require "containable"
+require "http"
+
+module Terminus
+  # Registers application dependencies.
+  module LibContainer
+    extend Containable
+
+    register :http do
+      ::HTTP.default_options = ::HTTP::Options.new features: {logging: {logger: self[:logger]}}
+      ::HTTP
+    end
+
+    register(:logger) { Cogger.new id: :terminus, formatter: :json }
+  end
+end
