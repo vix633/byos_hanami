ce86fa18fab39a8d8f09aa38c932b1eb88af7a77
Brooke Kuhlmann <brooke@alchemists.io>
2025-04-02 15:19:48 -0600

    Added screen downloader
    Necesary to download a screen for a device.
    
    Milestone: minor

---
+++ app/aspects/screens/downloader.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Terminus
+  module Aspects
+    module Screens
+      # A basic file downloader for screen images.
+      class Downloader
+        include Deps[:settings]
+        include Dependencies[client: :http]
+        include Initable[offset: 10] # Seconds.
+        include Dry::Monads[:result]
+
+        using Refinements::Pathname
+
+        def call uri, file_name
+          at = oldest_at
+
+          get(uri).fmap do |content|
+            Pathname(settings.generated_root).join(file_name).write(content).touch at
+          end
+        end
+
+        def oldest_at
+          oldest_file = Pathname(settings.generated_root).files.min_by(&:mtime)
+
+          return Time.now unless oldest_file
+
+          oldest_file.mtime - offset
+        end
+
+        def get uri
+          client.get(uri)
+                .then do |response|
+                  response.status.success? ? Success(response) : Failure(response)
+                end
+        end
+      end
+    end
+  end
+end
---
+++ spec/app/aspects/screens/downloader_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require "hanami_helper"
+
+RSpec.describe Terminus::Aspects::Screens::Downloader do
+  using Refinements::Pathname
+
+  subject(:downloader) { described_class.new client: }
+
+  include_context "with temporary directory"
+
+  let(:client) { HTTP }
+  let(:settings) { Hanami.app[:settings] }
+
+  describe "#call" do
+    before { allow(settings).to receive(:generated_root).and_return temp_dir }
+
+    it "downloads file" do
+      downloader.call "https://usetrmnl.com/assets/mashups.png", "test.png"
+      expect(temp_dir.join("test.png").exist?).to be(true)
+    end
+
+    it "marks downloaded file older than oldest file" do
+      temp_dir.join("test.txt").write("test").touch Time.new(2000, 1, 1, 0, 0, 0)
+      downloader.call "https://usetrmnl.com/assets/mashups.png", "test.png"
+
+      expect(temp_dir.join("test.png").mtime.year).to eq(1999)
+    end
+
+    it "answers output path" do
+      result = downloader.call "https://usetrmnl.com/assets/mashups.png", "test.png"
+
+      expect(result).to be_success(temp_dir.join("test.png"))
+    end
+
+    it "answers failure when image can't be downloaded" do
+      code = downloader.call("https://test.io/bogus.png", "bogus.png").alt_map { it.status.code }
+      expect(code).to be_failure(404)
+    end
+  end
+end
