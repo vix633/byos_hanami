f50ee3c276f7e45bb8e93b77f5491e3ac9ac7eb3
Brooke Kuhlmann <brooke@alchemists.io>
2025-04-07 10:30:43 -0600

    Added firmware contract and response to endpoints container
    Necessary to define these dependencies for eventual injection.
    
    Milestone: minor

--- lib/terminus/endpoints/container.rb
+++ lib/terminus/endpoints/container.rb
@@ -13,11 +13,13 @@ module Terminus
       namespace :contracts do
         register :current_screen, CurrentScreen::Contract
         register :display, Display::Contract
+        register :firmware, Firmware::Contract
       end
 
       namespace :responses do
         register :current_screen, CurrentScreen::Response
         register :display, Display::Response
+        register :firmware, Firmware::Response
       end
     end
   end
