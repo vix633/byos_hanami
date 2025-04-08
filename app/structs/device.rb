06e9aaee07e42833545b78a609001028470eef9f
Brooke Kuhlmann <brooke@alchemists.io>
2025-04-08 15:52:48 -0600

    Updated device struct to include firmware update
    Necessary to translate device attribute into API attribute that a device can understand when determining if firmware should be updated or not. This will soon be used by the API Display show action.
    
    Milestone: minor

--- app/structs/device.rb
+++ app/structs/device.rb
@@ -4,7 +4,9 @@ module Terminus
   module Structs
     # The device struct.
     class Device < DB::Struct
-      def as_api_display = {image_url_timeout: image_timeout, refresh_rate:}
+      def as_api_display
+        {image_url_timeout: image_timeout, refresh_rate:, update_firmware: firmware_update}
+      end
     end
   end
 end
--- spec/app/structs/device_spec.rb
+++ spec/app/structs/device_spec.rb
@@ -7,7 +7,11 @@ RSpec.describe Terminus::Structs::Device, :db do
 
   describe "#as_api_display" do
     it "answers display specific attributes" do
-      expect(device.as_api_display).to eq(image_url_timeout: 10, refresh_rate: 20)
+      expect(device.as_api_display).to eq(
+        image_url_timeout: 10,
+        refresh_rate: 20,
+        update_firmware: false
+      )
     end
   end
 end
