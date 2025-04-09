5316fab158533d99167b4d4ba899ced1a06fba70
Brooke Kuhlmann <brooke@alchemists.io>
2025-04-09 14:15:01 -0600

    Added device struct slug
    Necessary to uniquely identify a device. This is primarily intended for device specific sub-directories but might serve other uses too.
    
    Milestone: minor

--- app/structs/device.rb
+++ app/structs/device.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "dry/core"
+
 module Terminus
   module Structs
     # The device struct.
@@ -7,6 +9,8 @@ module Terminus
       def as_api_display
         {image_url_timeout: image_timeout, refresh_rate:, update_firmware: firmware_update}
       end
+
+      def slug = mac_address.tr ":", Dry::Core::EMPTY_STRING
     end
   end
 end
--- spec/app/structs/device_spec.rb
+++ spec/app/structs/device_spec.rb
@@ -3,7 +3,9 @@
 require "hanami_helper"
 
 RSpec.describe Terminus::Structs::Device, :db do
-  subject(:device) { Factory[:device, image_timeout: 10, refresh_rate: 20] }
+  subject :device do
+    Factory[:device, image_timeout: 10, mac_address: "AA:BB:CC:11:22:33", refresh_rate: 20]
+  end
 
   describe "#as_api_display" do
     it "answers display specific attributes" do
@@ -12,6 +14,12 @@ RSpec.describe Terminus::Structs::Device, :db do
         refresh_rate: 20,
         update_firmware: false
       )
+    end
+  end
+
+  describe "#slug" do
+    it "answers string with no colons" do
+      expect(device.slug).to eq("AABBCC112233")
     end
   end
 end
