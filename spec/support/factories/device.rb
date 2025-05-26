0e702e87161296e7975d2c31806b49a2622c02cf
Brooke Kuhlmann <brooke@alchemists.io>
2025-05-26 15:00:52 -0600

    Added device factory battery, WiFi, and firmware version attributes
    Necessary for testing purposes to have more attributes with default values.
    
    Milestone: minor

--- spec/support/factories/device.rb
+++ spec/support/factories/device.rb
@@ -5,6 +5,9 @@ Factory.define :device do |factory|
   factory.label "Test"
   factory.api_key "abc123"
   factory.mac_address "A1:B2:C3:D4:E5:F6"
+  factory.battery 3.0
+  factory.wifi(-44)
+  factory.firmware_version "1.2.3"
   factory.proxy false
   factory.created_at { Time.now }
   factory.updated_at { Time.now }
