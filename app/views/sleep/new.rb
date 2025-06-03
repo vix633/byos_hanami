17eed2e1fc9cb6fdc95dcf12329d1fc791f9bd01
Brooke Kuhlmann <brooke@alchemists.io>
2025-06-03 15:11:01 -0600

    Added sleep view and templates
    Necessary to render the sleep image for a device. This comes with it's own layout for futher customization if desired.
    
    Issue: 109
    Milestone: minor

---
+++ app/views/sleep/new.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Terminus
+  module Views
+    module Sleep
+      # The new view.
+      class New < Terminus::View
+        config.layout = "sleep"
+      end
+    end
+  end
+end
