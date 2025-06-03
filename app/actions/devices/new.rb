3aa98e6dd9b67bc8ebd35e60bdaf05e0373cc71f
Brooke Kuhlmann <brooke@alchemists.io>
2025-06-03 13:09:30 -0600

    Fixed device new action and view field defaults
    Necessary to ensure defaults are supplied to the view via the controller. The mistake was thinking you can use a block for the defaults in the view but a block only lazy loads values as actual values (not defaults). This prevents the issue of erasing manually entered values when the form has errors.
    
    Milestone: minor

--- app/actions/devices/new.rb
+++ app/actions/devices/new.rb
@@ -5,11 +5,13 @@ module Terminus
     module Devices
       # The new action.
       class New < Terminus::Action
+        include Deps["aspects.devices.builder"]
+
         def handle request, response
           view_settings = {}
           view_settings[:layout] = false if request.env.key? "HTTP_HX_REQUEST"
 
-          response.render view, **view_settings
+          response.render view, **view_settings, fields: builder.call
         end
       end
     end
--- app/views/devices/new.rb
+++ app/views/devices/new.rb
@@ -5,10 +5,8 @@ module Terminus
     module Devices
       # The new view.
       class New < Terminus::View
-        include Deps[builder: "aspects.devices.builder"]
-
         expose :device
-        expose(:fields) { builder.call }
+        expose :fields, default: Dry::Core::EMPTY_HASH
         expose :errors, default: Dry::Core::EMPTY_HASH
       end
     end
