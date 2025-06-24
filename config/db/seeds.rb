71a999aacd2d0a1c7b433b0d23185d803f8862dd
Brooke Kuhlmann <brooke@alchemists.io>
2025-06-24 13:57:26 -0600

    Added model seeds
    Necessary to populate databsae with default records. This model information will be used by devices and screens.
    
    Milestone: minor

---
+++ config/db/seeds.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+repository = Hanami.app["repositories.model"]
+
+repository.find_or_create :name,
+                          "t1",
+                          label: "T1",
+                          description: "The first production model.",
+                          width: 800,
+                          height: 480,
+                          published_at: Time.new(2024, 6, 25, 0, 0, 0)
