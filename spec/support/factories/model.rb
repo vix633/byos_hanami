5ba0a841c5aa1351f8113d1e7db740d9333ecb67
Brooke Kuhlmann <brooke@alchemists.io>
2025-06-24 14:05:14 -0600

    Added RSpec model factory
    Necessary to quickly create a model for testing purpposes.
    
    Milestone: minor

---
+++ spec/support/factories/model.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+Factory.define :model, relation: :model do |factory|
+  factory.sequence(:label) { "T#{it}" }
+  factory.sequence(:name) { "t#{it}" }
+  factory.width 800
+  factory.height 480
+  factory.published_at { Time.now }
+  factory.created_at { Time.now }
+  factory.updated_at { Time.now }
+end
