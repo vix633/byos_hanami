40229cb5503830a24b03f08c9269b28fd02101fd
Brooke Kuhlmann <brooke@alchemists.io>
2025-06-24 14:08:41 -0600

    Added model repository
    Necessary to manage records within repository.
    
    Milestone: minor

---
+++ app/repositories/model.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Terminus
+  module Repositories
+    # The model repository.
+    class Model < DB::Repository[:model]
+      commands :create, update: :by_pk, delete: :by_pk
+
+      def all
+        model.order { published_at.asc }
+             .to_a
+      end
+
+      def find(id) = (model.by_pk(id).one if id)
+
+      def find_by_name(value) = model.where(name: value).one
+
+      def find_or_create(key, value, **)
+        model.where(key => value).one.then { |record| record || create(name: value, **) }
+      end
+    end
+  end
+end
---
+++ spec/app/repositories/model_spec.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require "hanami_helper"
+
+RSpec.describe Terminus::Repositories::Model, :db do
+  subject(:repository) { described_class.new }
+
+  let(:model) { Factory[:model] }
+
+  describe "#all" do
+    it "answers all records by published date/time" do
+      model
+      two = Factory[:model, name: "two"]
+
+      expect(repository.all).to eq([model, two])
+    end
+
+    it "answers empty array when records don't exist" do
+      expect(repository.all).to eq([])
+    end
+  end
+
+  describe "#find" do
+    it "answers record by ID" do
+      expect(repository.find(model.id)).to eq(model)
+    end
+
+    it "answers nil for unknown ID" do
+      expect(repository.find(13)).to be(nil)
+    end
+
+    it "answers nil for nil ID" do
+      expect(repository.find(nil)).to be(nil)
+    end
+  end
+
+  describe "#find_by_name" do
+    it "answers record when found" do
+      expect(repository.find_by_name(model.name)).to eq(model)
+    end
+
+    it "answers nil when not found" do
+      expect(repository.find_by_name("bogus")).to be(nil)
+    end
+
+    it "answers nil for nil" do
+      expect(repository.find_by_name(nil)).to be(nil)
+    end
+  end
+
+  describe "#find_or_create" do
+    it "answers existing record" do
+      model
+      record = repository.find_or_create :name, model.name, label: "Upsert"
+
+      expect(record).to eq(model)
+    end
+
+    it "creates new record when record doesn't exist" do
+      creation = repository.find_or_create :name, "test", label: "Upsert", width: 1, height: 1
+      expect(creation).to have_attributes(name: "test", label: "Upsert", width: 1, height: 1)
+    end
+  end
+end
