7b2f7d84ba0b39a1031e15e447a792caba369c1a
Brooke Kuhlmann <brooke@alchemists.io>
2025-06-26 15:03:53 -0600

    Added playlist repository
    Necessary to manage playlist records with minimal functionality.
    
    Milestone: minor

---
+++ app/repositories/playlist.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Terminus
+  module Repositories
+    # The playlist repository.
+    class Playlist < DB::Repository[:playlist]
+      commands :create, update: :by_pk, delete: :by_pk
+
+      def all
+        playlist.order { created_at.asc }
+                .to_a
+      end
+
+      def find(id) = (playlist.by_pk(id).one if id)
+
+      def find_by(**) = playlist.where(**).first
+    end
+  end
+end
---
+++ spec/app/repositories/playlist_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require "hanami_helper"
+
+RSpec.describe Terminus::Repositories::Playlist, :db do
+  subject(:repository) { described_class.new }
+
+  let(:playlist) { Factory[:playlist] }
+
+  describe "#all" do
+    it "answers all records by created date/time" do
+      playlist
+      two = Factory[:playlist, name: "two"]
+
+      expect(repository.all).to eq([playlist, two])
+    end
+
+    it "answers empty array when records don't exist" do
+      expect(repository.all).to eq([])
+    end
+  end
+
+  describe "#find" do
+    it "answers record by ID" do
+      expect(repository.find(playlist.id)).to eq(playlist)
+    end
+
+    it "answers nil for unknown ID" do
+      expect(repository.find(666)).to be(nil)
+    end
+
+    it "answers nil for nil ID" do
+      expect(repository.find(nil)).to be(nil)
+    end
+  end
+
+  describe "#find_by" do
+    it "answers record when found by single attribute" do
+      expect(repository.find_by(name: playlist.name)).to eq(playlist)
+    end
+
+    it "answers record when found by multiple attributes" do
+      expect(repository.find_by(name: playlist.name, label: playlist.label)).to eq(playlist)
+    end
+
+    it "answers nil when not found" do
+      expect(repository.find_by(name: "bogus")).to be(nil)
+    end
+
+    it "answers nil for nil" do
+      expect(repository.find_by(name: nil)).to be(nil)
+    end
+  end
+end
