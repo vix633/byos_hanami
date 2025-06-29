7099aaa7aabc62d79cd54e8ea989d2071f1997a4
Brooke Kuhlmann <brooke@alchemists.io>
2025-06-29 08:52:24 -0600

    Added device patch schema
    Necessary to define the schema for device patches. Initially, this'll be used for API purposes since making HTTP PATCH requests may be a single attribute update or multiple attribute updates.
    
    Milestone: minor

---
+++ app/schemas/devices/patch.rb
@@ -0,0 +1,27 @@
+# auto_register: false
+# frozen_string_literal: true
+
+module Terminus
+  module Schemas
+    module Devices
+      # Defines device patch schema.
+      Patch = Dry::Schema.Params do
+        optional(:model_id).filled :integer
+        optional(:playlist_id).filled :integer
+        optional(:friendly_id).filled :string
+        optional(:label).filled :string
+        optional(:mac_address).filled Types::MACAddress
+        optional(:api_key).filled :string
+        optional(:refresh_rate).filled { int? > gteq?(10) }
+        optional(:image_timeout).filled { int? > gteq?(0) }
+        optional(:proxy).filled :bool
+        optional(:firmware_beta).filled :bool
+        optional(:firmware_update).filled :bool
+        optional(:sleep_start_at).maybe :time
+        optional(:sleep_stop_at).maybe :time
+
+        after(:value_coercer, &Coercers::DeviceValue)
+      end
+    end
+  end
+end
