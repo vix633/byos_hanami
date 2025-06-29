d9f1d0b974a58625c8c15227ec546dec6b4227e9
Brooke Kuhlmann <brooke@alchemists.io>
2025-06-29 08:20:09 -0600

    Added sleep stop at contract rule
    Necessary to allow this rule resuable within multiple contracts.
    
    Milestone: minor

---
+++ app/contracts/rules/sleep_stop_at.rb
@@ -0,0 +1,18 @@
+# auto_register: false
+# frozen_string_literal: true
+
+module Terminus
+  module Contracts
+    module Rules
+      SleepStopAt = lambda do
+        start_at = values.dig :device, :sleep_start_at
+
+        if value && start_at && value < start_at then key.failure "must be after start time"
+        elsif value && start_at.nil? then key.failure "must have corresponding start time"
+        elsif value.nil? && start_at then key.failure "must be filled"
+        else next
+        end
+      end
+    end
+  end
+end
---
+++ spec/app/contracts/rules/sleep_stop_at_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require "hanami_helper"
+
+RSpec.describe Terminus::Contracts::Rules::SleepStopAt do
+  subject(:contract) { simulation.new }
+
+  let :simulation do
+    implementation = described_class
+
+    Class.new Dry::Validation::Contract do
+      params do
+        required(:device).hash do
+          optional(:sleep_start_at).maybe :time
+          optional(:sleep_stop_at).maybe :time
+        end
+      end
+
+      rule(device: :sleep_stop_at, &implementation)
+    end
+  end
+
+  describe "#call" do
+    let :attributes do
+      {
+        device: {
+          sleep_start_at: "01:00:00",
+          sleep_stop_at: "02:00:00"
+        }
+      }
+    end
+
+    it "answers success with no errors" do
+      result = contract.call attributes
+      expect(result.success?).to be(true)
+    end
+
+    it "answers error when start isn't before stop" do
+      attributes[:device].merge! sleep_start_at: "02:00:00", sleep_stop_at: "01:00:00"
+      result = contract.call attributes
+
+      expect(result.errors.to_h).to eq(device: {sleep_stop_at: ["must be after start time"]})
+    end
+
+    it "answers error when corresponding start is missing" do
+      attributes[:device].delete :sleep_start_at
+      result = contract.call attributes
+
+      expect(result.errors.to_h).to eq(
+        device: {
+          sleep_stop_at: ["must have corresponding start time"]
+        }
+      )
+    end
+
+    it "answers error when stop isn't filled" do
+      attributes[:device].delete :sleep_stop_at
+      result = contract.call attributes
+
+      expect(result.errors.to_h).to eq(device: {sleep_stop_at: ["must be filled"]})
+    end
+  end
+end
