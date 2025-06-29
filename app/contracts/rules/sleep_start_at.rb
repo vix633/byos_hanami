44d4b3512687143622c7551d329981adfbd99faf
Brooke Kuhlmann <brooke@alchemists.io>
2025-06-29 08:12:02 -0600

    Added sleep start at contract rule
    Necessary to allow this rule resuable within multiple contracts.
    
    Milestone: minor

---
+++ app/contracts/rules/sleep_start_at.rb
@@ -0,0 +1,18 @@
+# auto_register: false
+# frozen_string_literal: true
+
+module Terminus
+  module Contracts
+    module Rules
+      SleepStartAt = lambda do
+        stop_at = values.dig :device, :sleep_stop_at
+
+        if value && stop_at && value > stop_at then key.failure "must be before stop time"
+        elsif value && stop_at.nil? then key.failure "must have corresponding stop time"
+        elsif value.nil? && stop_at then key.failure "must be filled"
+        else next
+        end
+      end
+    end
+  end
+end
