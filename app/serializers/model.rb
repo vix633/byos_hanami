19bf9d18ccfffa5747d57c9782c7203456ff9288
Brooke Kuhlmann <brooke@alchemists.io>
2025-07-21 17:59:20 -0600

    Updated model serializer to process additional attributes
    Necessary to process newly added attributes.
    
    Milestone: minor

--- app/serializers/model.rb
+++ app/serializers/model.rb
@@ -5,7 +5,22 @@ module Terminus
   module Serializers
     # A model serializer for specific keys.
     class Model
-      KEYS = %i[id name label description width height published_at].freeze
+      KEYS = %i[
+        id
+        name
+        label
+        description
+        mime_type
+        colors
+        bit_depth
+        scale_factor
+        rotation
+        offset_x
+        offset_y
+        width
+        height
+        published_at
+      ].freeze
 
       def initialize record, keys: KEYS, transformer: Transformers::Time
         @record = record
--- spec/app/serializers/model_spec.rb
+++ spec/app/serializers/model_spec.rb
@@ -12,6 +12,13 @@ RSpec.describe Terminus::Serializers::Model, :db do
       name: "t1",
       label: "T1",
       description: nil,
+      mime_type: "image/bmp",
+      colors: 4,
+      bit_depth: 2,
+      scale_factor: 2,
+      rotation: 90,
+      offset_x: 10,
+      offset_y: 15,
       width: 800,
       height: 480,
       published_at: "2025-01-01T00:00:00+0000"
