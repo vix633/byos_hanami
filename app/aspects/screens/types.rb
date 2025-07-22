6f9476dd0fc8479ffe0d4e0897cded247da23ffb
Brooke Kuhlmann <brooke@alchemists.io>
2025-07-22 10:36:38 -0600

    Updated screen types to aspects namespace
    Necesary to move into the main app for further use. This also switches to using MIME types for lookup since we'll be using model records to dictate how screens are processed.
    
    _This is a breaking change_ that will require all upstream objects to be updated shortly.
    
    Milestone: minor

---
+++ app/aspects/screens/types.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Terminus
+  module Aspects
+    module Screens
+      # Defines supported MIME types along with corresponding MiniMagick version types.
+      TYPES = {"image/bmp" => "bmp3", "image/png" => "png"}.freeze
+    end
+  end
+end
--- lib/terminus/screens/types.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Terminus
-  module Screens
-    # Defines supported image types along with corresponding MiniMagick version types.
-    TYPES = {"bmp" => "bmp3", "png" => "png"}.freeze
-  end
-end
