f5c39ac18a68d37d1e7d0ef7edfba37a52161cfc
Brooke Kuhlmann <brooke@alchemists.io>
2025-07-22 11:26:06 -0600

    Updated unprocessed screen saver as aspect
    Necessary to modify the original implementation by ensuring images are attached to screens instead of stored in the public folder.
    
    _This is a breaking change_ that will require the Screens API create action to be updated shortly.
    
    Milestone: minor

---
+++ app/aspects/screens/savers/unprocessed.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require "dry/monads"
+require "mini_magick"
+
+module Terminus
+  module Aspects
+    module Screens
+      module Savers
+        # Saves URI as a processed image.
+        class Unprocessed
+          include Deps["aspects.screens.converter", repository: "repositories.screen"]
+          include Dry::Monads[:result]
+
+          def initialize(client: MiniMagick::Image, struct: Terminus::Structs::Screen.new, **)
+            @client = client
+            @struct = struct
+            super(**)
+          end
+
+          def call(payload) = Pathname.mktmpdir { process payload, it }
+
+          private
+
+          attr_reader :client, :struct
+
+          def process payload, directory
+            input_path = Pathname(directory).join "input.png"
+
+            client.open(payload.content)
+                  .write(input_path)
+                  .then { convert payload.model, input_path, directory.join(payload.filename) }
+                  .bind { |path| save payload, path }
+          end
+
+          def convert model, input_path, output_path
+            converter.call model, input_path, output_path
+          end
+
+          def save payload, path
+            struct = attach payload, path
+            Success repository.create(image_data: struct.image_attributes, **payload.attributes)
+          rescue ROM::SQL::Error => error
+            Failure error.message
+          end
+
+          def attach payload, path
+            path.open { |io| struct.upload io, metadata: {"filename" => payload.filename} }
+            struct
+          end
+        end
+      end
+    end
+  end
+end
