34979da658aa62d44121cbb71fca968303a429f6
Brooke Kuhlmann <brooke@alchemists.io>
2025-07-25 17:01:38 -0600

    Updated screen rotator to calculate device playlist's next screen
    Necessary to answer a screen instead of an image since we are using playlist items with screen attachments. Image encryption has been removed in favor of having a separate object handle this (soon to be implemented).
    
    Milestone: minor

--- app/aspects/screens/rotator.rb
+++ app/aspects/screens/rotator.rb
@@ -1,18 +1,50 @@
 # frozen_string_literal: true
 
-require "initable"
+require "dry/monads"
 
 module Terminus
   module Aspects
     module Screens
-      # Rotates and fetches images for rendering on a device.
+      # Updates a device's current playlist item by rotating to next screen.
       class Rotator
-        include Deps[:settings, "aspects.screens.fetcher"]
-        include Initable[toucher: proc { Terminus::Screens::Toucher }]
+        include Deps[
+          "aspects.screens.sleeper",
+          playlist_repository: "repositories.playlist",
+          item_repository: "repositories.playlist_item"
+        ]
+        include Dry::Monads[:result]
 
-        def call device, encryption: nil
-          fetcher.call(device, encryption:).tap do
-            toucher.call settings.screens_root.join(device.slug)
+        def call device
+          if device.asleep?
+            sleeper.call device
+          else
+            find_playlist(device.playlist_id).fmap { |playlist| update_current_item playlist }
+                                             .bind { |item_id| find_screen item_id }
+          end
+        end
+
+        private
+
+        def find_playlist id
+          playlist = playlist_repository.find id
+
+          return Success playlist if playlist
+
+          Failure "Unable to rotate screen. Can't find playlist with ID: #{id.inspect}."
+        end
+
+        def update_current_item playlist
+          playlist_id = playlist.id
+          next_item = item_repository.next_item(after: playlist.current_item_position, playlist_id:)
+          next_item_id = next_item.id if next_item
+
+          playlist_repository.update playlist_id, current_item_id: next_item_id
+          next_item_id
+        end
+
+        def find_screen item_id
+          item_repository.find(item_id).then do |item|
+            item ? Success(item.screen) : Failure("Unable to rotate screen. Playlist has no items.")
           end
         end
       end
--- spec/app/aspects/screens/rotator_spec.rb
+++ spec/app/aspects/screens/rotator_spec.rb
@@ -3,46 +3,64 @@
 require "hanami_helper"
 
 RSpec.describe Terminus::Aspects::Screens::Rotator, :db do
-  using Refinements::Pathname
-
-  subject(:rotator) { described_class.new settings: }
-
-  include_context "with main application"
+  subject(:rotator) { described_class.new }
 
   describe "#call" do
-    let(:device) { Factory[:device] }
-
-    before do
-      temp_dir.join("#{device.slug}/one.bmp").make_ancestors.touch
-      temp_dir.join("#{device.slug}/two.bmp").make_ancestors.touch
+    let :device do
+      provisioner.call(model_id: Factory[:model].id, mac_address: "A1:B2:C3:D4:E5:F6").value!
     end
 
-    it "answers oldest image" do
-      skip "Doesn't work on CI." if ENV["CI"]
+    let(:provisioner) { Terminus::Aspects::Devices::Provisioner.new }
+    let(:playlist_repository) { Terminus::Repositories::Playlist.new }
+    let(:item_repository) { Terminus::Repositories::PlaylistItem.new }
 
-      expect(rotator.call(device)).to eq(
-        filename: "two.bmp",
-        image_url: "https://localhost/assets/screens/A1B2C3D4E5F6/two.bmp"
+    it "answers sleep screen when device is asleep" do
+      allow(device).to receive(:asleep?).and_return true
+      expect(rotator.call(device).success).to have_attributes(label: /Sleep/)
+    end
+
+    it "answers current screen when playlist has single item" do
+      expect(rotator.call(device).success).to have_attributes(label: /Welcome/)
+    end
+
+    it "answers next screen when current screen isn't last" do
+      Factory[
+        :playlist_item,
+        playlist_id: device.playlist_id,
+        screen_id: Factory[:screen, label: "Test"].id,
+        position: 2
+      ]
+
+      expect(rotator.call(device).success).to have_attributes(label: "Test")
+    end
+
+    it "answers first screen when current screen is last screen" do
+      screen = Factory[:screen, label: "Test"]
+
+      item = Factory[
+        :playlist_item,
+        playlist_id: device.playlist_id,
+        screen_id: screen.id,
+        position: 2
+      ]
+
+      playlist_repository.update device.playlist_id, current_item_id: item.id
+
+      expect(rotator.call(device).success).to have_attributes(label: /Welcome/)
+    end
+
+    it "answers failure when playlist can't be found" do
+      expect(rotator.call(Factory[:device])).to be_failure(
+        "Unable to rotate screen. Can't find playlist with ID: nil."
       )
     end
 
-    it "answers oldest image with encryption" do
-      skip "Doesn't work on CI." if ENV["CI"]
+    it "answers failure when playlist is empty" do
+      playlist = Factory[:playlist]
+      device = Factory[:device, playlist_id: playlist.id]
 
-      expect(rotator.call(device, encryption: :base_64)).to eq(
-        filename: "two.bmp",
-        image_url: "data:image/bmp;base64,"
-      )
-    end
-
-    it "updates oldest image as newest image" do
-      rotator.call device
-
-      expect(temp_dir.join("A1B2C3D4E5F6").files.sort_by(&:mtime)).to eq(
-        [
-          temp_dir.join("A1B2C3D4E5F6/two.bmp"),
-          temp_dir.join("A1B2C3D4E5F6/one.bmp")
-        ]
+      expect(rotator.call(device)).to be_failure(
+        "Unable to rotate screen. Playlist has no items."
       )
     end
   end
